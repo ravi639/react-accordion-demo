fb5c944c33a5f36a9ca4a338ed2b74e1571ddff5
ravi639 <115543344+ravi639@users.noreply.github.com>
2023-05-25 16:10:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import Accordion from "./Accordion";
+const App = () => {
+  const accordionData = [
+    {
+      title: 'What is your return policy?',
+      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+      laborum cupiditate possimus labore, hic temporibus velit dicta earum
+      suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+      voluptatem.`
+    },
+    {
+      title: 'How do I track my order?',
+      content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
+      reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
+      quaerat iure quos dolorum accusantium ducimus in illum vero commodi
+      pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
+      quidem maiores doloremque est numquam praesentium eos voluptatem amet!
+      Repudiandae, mollitia id reprehenderit a ab odit!`
+    },
+    {
+      title: 'Can I purchase items again?',
+      content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
+      quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
+      dolor ut sequi minus iste? Quas?`
+    }
+  ];
+
+  return (
+    <div>
+      <h1>React Accordion Demo</h1>
+      <div className="accordion">
+        {accordionData.map(({ title, content }) => (
+          <Accordion title={title} content={content} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles.css';
+import react, { useState } from 'react';
+import Accordion from './Accordion';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+// const [isActive, setIsActive] = useState(false); 
+
+
